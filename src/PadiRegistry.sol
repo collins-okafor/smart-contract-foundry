9f01abb78beff2c9941180c41414e65aabbe196b
Collins Okafor <collinszurum@gmail.com>
2023-11-12 11:30:52 +0100

    padiregistry

--- src/PadiRegistry.sol
+++ src/PadiRegistry.sol
@@ -1,5 +1,34 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
+import "openzeppelin-contracts/utils/cryptography/ECDSA.sol";
+
+interface IPFS {
+    function upload(string memory _data) external returns (bytes32);
+}
+
+contract PassportRegistry is Ownable {
+    using ECDSA for bytes32;
+
+    // Structure to represent user data
+    struct UserData {
+        string passportNumber;
+        address userAddress;
+    }
+
+    // Mapping from unique ID to user data
+    mapping(bytes32 => UserData) public userData;
+
+    // Mapping from user address to verifier addresses
+    mapping(address => address[]) public verifiers;
+
+    // Mapping from verifier address to charges
+    mapping(address => uint256) public verificationCharges;
+
+    // Mapping from address to tag (0 for user, 1 for padi, 2 for verifier)
+    mapping(address => uint8) public addressTags;
+
+    // IPFS contract address
+    address public ipfsContractAddress;
+}
