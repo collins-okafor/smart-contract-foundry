e5ccaedb1fb80053bae99dc02e8eaf5b01100987
Collins Okafor <collinszurum@gmail.com>
2023-11-12 11:22:41 +0100

    Added spacebear contract and removed everything else

--- src/PadiRegistry.sol
+++ src/PadiRegistry.sol
@@ -1,2 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
